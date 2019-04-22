661f30ce16e8976811918a655828ef0f783de00a
Anam Mohammad <m.anam0603@gmail.com>
2019-04-21 21:28:20 -0500

    Updates

---
+++ precache-manifest.b192aaefe13ea212cedca6a78020dfab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "870aa7f558dc74c617e7",
+    "url": "/clicky-game/static/css/main.a73d6047.chunk.css"
+  },
+  {
+    "revision": "870aa7f558dc74c617e7",
+    "url": "/clicky-game/static/js/main.512ca49f.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "4743c039c04edea7e174",
+    "url": "/clicky-game/static/js/2.1db7aab6.chunk.js"
+  },
+  {
+    "revision": "bb315ce3a7cbab14c7bd076166589129",
+    "url": "/clicky-game/static/media/background.bb315ce3.png"
+  },
+  {
+    "revision": "68c2bcbcf1f980ad5645b4107c0123ba",
+    "url": "/clicky-game/index.html"
+  }
+];
